9808a6f50c59a4870995ea7b35398d32ba4f8fbf
Adam Keenan <adam.g.keenan@gmail.com>
2014-10-09 09:08:58 +0000

    Apparently did the config wrong: no port needed

--- app/config/database.php
+++ app/config/database.php
@@ -1,5 +1,10 @@
 <?php
-$dbopts = parse_url(getenv('DATABASE_URL'));
+$url = parse_url(getenv("DATABASE_URL"));
+
+$host = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$database = substr($url["path"], 1);
 return array(
 
 	/*
@@ -65,10 +70,10 @@ return array(
 
 		'pgsql' => array(
 			'driver'   => 'pgsql',
-			'host'     => $dbopts["host"].":".$dbopts["port"],
-			'database' => ltrim($dbopts["path"], '/'),
-			'username' => $dbopts["user"],
-			'password' => $dbopts["pass"],
+			'host'     => $host,
+			'database' => $database,
+			'username' => $username,
+			'password' => $password,
 			'charset'  => 'utf8',
 			'prefix'   => '',
 			'schema'   => 'public',
